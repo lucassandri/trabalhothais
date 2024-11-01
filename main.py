89e983a65609330fcccf5525b26eb1207820b285
lucassandri <81128429+lucassandri@users.noreply.github.com>
2024-11-01 00:10:21 -0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from controlador_sistema import ControladorSistema
-
-if __name__ == "__main__":
-    print('Seja bem vindo(a)!')
-    controlador = ControladorSistema('')
-    controlador.inicializa_sistema()
