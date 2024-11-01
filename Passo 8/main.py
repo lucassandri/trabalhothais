15b949e39f41043058dca50847da4599ec672e34
lucassandri <81128429+lucassandri@users.noreply.github.com>
2024-11-01 04:31:49 -0300

    Add files via upload

---
+++ Passo 8/main.py
@@ -0,0 +1,6 @@
+from controladores.controlador_sistema import ControladorSistema
+
+if __name__ == "__main__":
+    print('Seja bem vindo(a)!')
+    controller = ControladorSistema('')
+    controller.inicializa_sistema()
---
+++ Passo 8/telas/tela_professor.py
@@ -0,0 +1,31 @@
+from telas.tela_abstrata import TelaAbstrata
+
+class TelaProfessor(TelaAbstrata):
+
+    def __init__(self):
+        pass
+    
+    def tela_opcoes(self):
+        print("-------- PROFESSORES ----------")
+        print("1 - Incluir Professor")
+        print("2 - Alterar Professor")
+        print("3 - Excluir Professor")
+        print("4 - Listar Professores")
+        print("5 - Mostrar professor por email")
+        print("0 - Retornar")
+
+        opcao = self.le_num_inteiro("Escolha a opcao:", [0,1,2,3,4,5])
+        return opcao
+    
+    def pega_dados_professor(self):
+        print("-------- DADOS PROFESSOR ----------")
+        nome = self.le_string("Nome: ")
+        salario = self.le_float("Salário: ")
+        leciona = self.le_string("Lecionando: ")
+        email = self.le_string("Email: ")
+        
+        return {"nome": nome, "salario": salario, "leciona": leciona, "email": email}
+    
+    def seleciona_professor(self):
+        email = self.le_string("Digite o email do professor: ")
+        return email
