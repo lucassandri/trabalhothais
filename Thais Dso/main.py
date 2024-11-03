956ccc37e8e0a9c813f0a50081bd3cd377935040
GuilhermeSteinerUFSC <portaldoscalouros@gmail.com>
2024-11-03 19:24:25 -0300

    Delete Thais Dso directory

--- Thais Dso/main.py
+++
@@ -1,6 +0,0 @@
-from controladores.controlador_sistema import ControladorSistema
-
-if __name__ == "__main__":
-    print('Seja bem vindo(a)!')
-    controlador = ControladorSistema('')
-    controlador.inicializa_sistema()
